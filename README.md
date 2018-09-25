f6d89426872ea36be13806bd5d3f9ceef311d03b
NISH1001 <nishanpantha@gmail.com>
2018-09-25 19:31:50 +0545

    Add minimal implementation of markov chain

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # paradoxical-poem-generator
 A "very" naive poem generation.
+
+
+## Dependencies
+All you need is python3, and that's it. 
+
+
+## What's With The Repo?
+Oh yeah! My intention is to generate new texts/poems/lines based on my own poems as source.
+The texts are generated using [Markov Chain](https://en.wikipedia.org/wiki/Markov_chain).
+I have done a naive implementation of the Markov Chain using [trie](https://en.wikipedia.org/wiki/Trie) data structure 
+using python dictionaries.
---
+++ markov.py
@@ -0,0 +1,85 @@
+import random
+import re
+
+from collections import defaultdict
+
+class MarkovChain:
+    def __init__(self, lookback=2):
+        self.trie = defaultdict(lambda : defaultdict(int))
+        self.lookback = lookback
+        self.lines = []
+
+    def train(self, lines):
+        """
+            Build markov model
+        """
+        self.lines += lines
+        for title in lines:
+            tokens = title.split()
+            if len(tokens) > self.lookback:
+                for i in range(len(tokens) + 1):
+                    a = ' '.join(tokens[max(0, i-self.lookback) : i])
+                    b = ' '.join(tokens[i : i+1])
+                    self.trie[a][b] += 1
+        self._build_probabilities()
+
+    def _build_probabilities(self):
+        """
+            Calculate probabilities
+        """
+        for word, following in self.trie.items():
+            total = float(sum(following.values()))
+            for key in following:
+                following[key] /= total
+
+    def _sample(self, items):
+        next_word = None
+        t = 0.0
+        for k, v in items:
+            t += v
+            if t and random.random() < v/t:
+                next_word = k
+        return next_word
+
+    def generate(self, n):
+        sentences = []
+        while len(sentences) < n:
+            sentence = []
+            next_word = self._sample(self.trie[''].items())
+            while next_word != '':
+                sentence.append(next_word)
+                next_word = self._sample(self.trie[' '.join(sentence[-self.lookback:])].items())
+            sentence = ' '.join(sentence)
+            flag = True
+            for title in self.lines: #Prune lines that are substrings of actual lines
+                if sentence in title:
+                    flag = False
+                    break
+            if flag:
+                sentences.append(sentence)
+        return sentences
+
+def load_data(filename):
+    lines = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            if line:
+                lines.append(line.strip())
+    return lines
+
+
+def main():
+    data = load_data("data/poem")
+    mc = MarkovChain(lookback=1)
+    mc.train(data)
+
+    lines = mc.generate(10)
+    for line in lines:
+        print(line)
+
+
+if __name__ == "__main__":
+    main()
+
+
